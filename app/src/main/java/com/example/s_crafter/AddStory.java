5747aaa72a4c78a94c34d1b6a3a44972514a7e55
WenDeHor <mazur23252@gmail.com>
2024-08-13 17:05:31 +0300

    10.select photo

--- app/src/main/java/com/example/s_crafter/AddStory.java
+++ app/src/main/java/com/example/s_crafter/AddStory.java
@@ -1,18 +1,28 @@
 package com.example.s_crafter;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.InputFilter;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class AddStory extends AppCompatActivity {
+import java.io.IOException;
 
+public class AddStory extends AppCompatActivity {
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,9 +33,41 @@ public class AddStory extends AppCompatActivity {
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
 //        });
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+//        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         EditText editText = findViewById(R.id.addingText);
         int maxLength = 250;
         editText.setFilters(new InputFilter[] { new InputFilter.LengthFilter(maxLength) });
+
+        reloadSelectedImage();
+    }
+
+    private void reloadSelectedImage(){
+        Button buttonSelectImage = findViewById(R.id.buttonSelectImage);
+        imageView = findViewById(R.id.imageStoryPage);
+        buttonSelectImage.setOnClickListener(v -> openGallery());
+    }
+
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST
+                && resultCode == RESULT_OK
+                && data != null
+                && data.getData() != null) {
+            Uri imageUri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
